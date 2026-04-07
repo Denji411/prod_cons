514e24041683bdff1adb3d3ec8ba62fe1465da40
S10514961A <s10514961a@localhost.localdomain>
2026-04-07 11:07:02 +0200

    finalizzazione domande

--- src/consumatore.c
+++ src/consumatore.c
@@ -8,24 +8,25 @@
 void* consumatore(void*) {
 
     /*
-        Domanda 4.1: Alla prima iterazione, mutex_cons è acquisito e di conseguenza si blocca.
+        Domanda 4.1:
+            Alla prima iterazione, mutex_cons è acquisito e di conseguenza si blocca.
             Questo perché deve leggere il dato fornito dal produttore, e deve quindi aspettare che glielo fornisca.
 
         Domanda 4.2:
-            mutex_cons -> acquisito dal main immediatamente;
+            mutex_cons -> acquisito dal main immediatamente.
             prima iterazione:
-                mutex_prod -> acquisito dal produttore;
-                il produttore inserisce il dato;
-                mutex_cons -> rilasciato dal produttore - da il permesso al consumatore di fare lock;
-                inizia la seconda iterazione del produttore, ma dato che cerca di fare lock su un mutex già occupato, va in attesa;
+                mutex_prod -> acquisito dal produttore.
+                il produttore inserisce il dato.
+                mutex_cons -> rilasciato dal produttore - da il permesso al consumatore di fare lock.
+                inizia la seconda iterazione del produttore, ma dato che cerca di fare lock su un mutex già occupato, va in attesa.
 
-                mutex_cons -> acquisito dal consumatore;
-                il consumatore legge il dato;
-                mutex_prod -> rilasciato dal consumatore - da il permesso al produttore di fare lock;
-                inizia la seconda iterazione del consumatore, ma dato che cerca di fare lock su un mutex già occupato, va in attesa;
+                mutex_cons -> acquisito dal consumatore.
+                il consumatore legge il dato.
+                mutex_prod -> rilasciato dal consumatore - da il permesso al produttore di fare lock.
+                inizia la seconda iterazione del consumatore, ma dato che cerca di fare lock su un mutex già occupato, va in attesa.
             seconda iterazione: 
-                il produttore termina l'attesa e esegue la stessa cosa della prima iterazione;
-                il consumatore termina l'attesa e esegue la stessa cosa della prima iterazione;
+                il produttore termina l'attesa e esegue la stessa cosa della prima iterazione.
+                il consumatore termina l'attesa e esegue la stessa cosa della prima iterazione.
     */
     
     for (int i = 0; i < N_ELEMENTI; i++) {
--- src/main.c
+++ src/main.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
 
 #include "globals.h"
 #include "consumatore.h"
 #include "produttore.h"
+
+#define NS_PER_SECONDS 1000000000
+
+void sub_clocktime(struct timespec t1, struct timespec t2, struct timespec *td) {
+    td -> tv_nsec = t2.tv_nsec - t1.tv_nsec;
+    td -> tv_sec = t2.tv_sec - t1.tv_sec;
+
+    if (td -> tv_sec > 0 && td -> tv_nsec < 0) {
+        td -> tv_nsec += NS_PER_SECONDS;
+        td -> tv_sec--;
+    }
+
+    if (td -> tv_sec < 0 && td -> tv_nsec > 0) {
+        td -> tv_nsec -= NS_PER_SECONDS;
+        td -> tv_sec++;
+    }
+}
 
 int main() {
 
@@ -19,8 +37,11 @@ int main() {
     pthread_mutex_init(&mutex_prod, NULL);
     pthread_mutex_lock(&mutex_cons);
 
+    struct timespec start, finish, delta;
     pthread_t prod, cons;
     
+    clock_gettime(CLOCK_REALTIME, &start);
+
     if(pthread_create(&prod, NULL, produttore, NULL) != 0) {
         perror("Errore creazione produttore");
         return EXIT_FAILURE;
@@ -38,15 +59,32 @@ int main() {
     pthread_join(prod, NULL);
     pthread_join(cons, NULL);
 
+    clock_gettime(CLOCK_REALTIME, &finish);
+    sub_clocktime(start, finish, &delta);
+
+    printf("Durata scambio prod/cons: %ld.%.9ld\n", delta.tv_sec, delta.tv_nsec);
+
     pthread_mutex_destroy(&mutex_prod);
     pthread_mutex_destroy(&mutex_cons);
     return EXIT_SUCCESS;
 
     /*
-        Domanda 7.1: Non sarebbe possibile utilizzare questo metodo con due o più produttori perchè si presenterebbe una race condition sulla scrittura del dato.
+        Domanda 7.1:
+            Non sarebbe possibile utilizzare questo metodo con due o più produttori perchè si presenterebbe una race condition sulla scrittura del dato.
             Non si potrebbe inoltre determinare quale produttore viene lockato o unlockato.
 
         Domanda 7.2: 
+            1. Il produttore non aspetterebbe mai il consumatore e sfornerebbe prodotti non curante di se stanno venendo consumati;
+            2. Il conumatore non unlockerebbe mai, provocandogli uno stato di stallo perpetuo.
+
+        Domanda 7.3:
+            Il throughput massimo di questo sistema è di circa 1052 scambi al secondo.
+            Questo dipende dalla velocità di entrambi, si il produttore che il consumatore, di eseguire le loro operazioni.
+
+        Domanda 7.4:
+            Questa pratica si chiama rendez-vous e permette a due thread di aspettarsi a vicenda quando devono operare insieme.
+            Vantaggi: Evita problemi di condivisione dati tra threads, permette di evitare la gestione dei buffer.
+            Svantaggi: I threads non lavorano mai veramente in parallelo, ma uno aspetta sempre l'altro.
     */
 
 }
